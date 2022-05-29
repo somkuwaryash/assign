902f295cbb16db9f90c6f8d5529f2486c32d932d
somkuwaryash <63736665+somkuwaryash@users.noreply.github.com>
2022-05-29 22:46:00 +0530

    Create gnosiswithoppenzippline.sol

---
+++ gnosiswithoppenzippline.sol
@@ -0,0 +1,38 @@
+// gnosis is nultisig transaction safe, so I deployed this contract and changed the authority by transferring ownership
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+
+/// @custom:security-contact security@example.biz
+contract SomkuwarToken is ERC721, Pausable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("SomkuwarToken", "MTK") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+}
